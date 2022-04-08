6549876d0af9506b9935254e023f7e7abc4ff13b
jackie <bobbinnej@gmail.com>
2022-04-08 15:43:18 +0300

    Add bootstrap to application

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+ /*Import Quote blueprint class */
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  /*Lets create Sample Array of Quotes using the quote blueprint class */
+  /*Create an object quotes and attribute it to the Quote blueprint class 
+    then define an array of quotes with author and submitter
+  */
+  quotes:Quote[] =[
+
+    {description:'Momentum is power', author:'Nelson Mandela', submitter:'Jackline Njoroge'},
+    {description:'Setting an example is not the main means of influencing others, it is the only means ', author:'Albert Einstein', submitter:'Jackline Njoroge'},
+    {description:'Being a good example teaches others to be good', author:'Aesop', submitter:'Jackline Njoroge'},
+  ]
+ 
+  
 
   constructor() { }
 
