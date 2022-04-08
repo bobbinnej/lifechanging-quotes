662c7481a9676df5575f0d0a83a80bee8b6f301d
jackie <bobbinnej@gmail.com>
2022-04-08 13:07:03 +0300

    Nest quote components inside app component child of the root component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,8 @@
 export class Quote {
+    /*These are all the detalis we expect to be present in our Quote application.
+    A user will be able to add quotes that have author and submitter name below the quote 
+    */
+    description!:string;
+    author!:string;
+    submitter!:string;
 }
