3e1710f33fcab2c09d759acdacf7f1ac8a63b574
jackie <bobbinnej@gmail.com>
2022-04-09 18:31:39 +0300

    Create quote-detail component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,7 +2,13 @@ export class Quote {
     /*These are all the detalis we expect to be present in our Quote application.
     A user will be able to add quotes that have author and submitter name below the quote 
     */
-    description!:string;
-    author!:string;
-    submitter!:string;
+   
+    showAuthor: boolean;
+    showSubmitter:boolean;
+    constructor(public description:string, public author:string, public submitter:string ){
+        this.showAuthor=false;
+        this.showSubmitter=false;
+
+
+    }
 }
