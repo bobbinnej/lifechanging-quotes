c79251ca8764fb9a8976514259f0f0353510471a
jackie <bobbinnej@gmail.com>
2022-04-09 16:21:12 +0300

    Nest quote-form-component to to quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -17,7 +17,7 @@ export class QuoteComponent implements OnInit {
     {description:'Momentum is power', author:'Nelson Mandela', submitter:'Jackline Njoroge'},
     {description:'Setting an example is not the main means of influencing others, it is the only means ', author:'Albert Einstein', submitter:'Jackline Njoroge'},
     {description:'Being a good example teaches others to be good', author:'Aesop', submitter:'Jackline Njoroge'},
-  ]
+  ];
  
   
 
